ee2af0abc01d2f374ccc9f88cdd840a3a97c5e0e
Jdjohn307 <jdjohn307@gmail.com>
2025-08-28 15:40:12 -0700

    Add rescue for NotNullViolation to base controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,13 @@
 module Api
   class BaseController < ActionController::Base
     protect_from_forgery with: :null_session
+
+     rescue_from ActiveRecord::NotNullViolation, with: :handle_not_null_violation
+
+    private
+
+    def handle_not_null_violation(exception)
+      render json: { error: [{ title: 'Invalid Data', detail: exception.message }] }, status: :unprocessable_entity
+    end
   end
 end
