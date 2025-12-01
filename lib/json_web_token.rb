99e1004bee5972b23e2b41488cdf29d0c58f3828
Jdjohn307 <jdjohn307@gmail.com>
2025-11-30 16:23:36 -0800

    Fixing CI JWT token and secrets

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,18 +1,17 @@
 require "jwt"
 
 class JsonWebToken
- SECRET_KEY = ENV.fetch("JWT_SECRET") { Rails.application.credentials.jwt_secret }
-
+  SECRET_KEY = ENV["JWT_SECRET"].presence || Rails.application.credentials.jwt_secret || "test-fallback-secret"
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+    decoded = JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')[0]
     HashWithIndifferentAccess.new(decoded)
-  rescue JWT::DecodeError
+  rescue JWT::DecodeError, JWT::ExpiredSignature
     nil
   end
 end
